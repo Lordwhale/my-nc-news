97653d1ed6124eb4743b7e6bd136071e7068bfea
ZoltanM <zmozga@yahoo.co.uk>
2024-11-27 18:33:10 +0000

    taks 5 added comment_count

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -1,7 +1,11 @@
 const db = require("../connection");
 
 exports.selectArticle = () => {
-  return db.query("SELECT article_id, title,topic, author, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC")
+  return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments
+  ON articles.article_id = comments.article_id
+  GROUP BY articles.article_id ORDER BY articles.created_at DESC;`)
     .then(({ rows }) => {
       return rows;
     });
